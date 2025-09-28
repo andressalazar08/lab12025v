439e72465aa119f1240eba9a31f9478a8511b619
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2025-09-28 16:27:27 -0500

    inicio del proyecto

---
+++ src/main/java/com/udea/lab12025v/Lab12025vApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab12025v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab12025vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab12025vApplication.class, args);
+	}
+
+}
